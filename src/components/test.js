010ff4c8e59f22fb5ed6afc44af8b4f7fe1ece84
jv <joaovictor.bispo@novaescola.org.br>
2021-12-09 12:02:58 -0300

    cricacao do primeiro componente

---
+++ src/components/test.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { title } from './test.module.css';
+
+const TextComponent = () => {
+
+    return (
+        <h1 className={title}>title</h1>
+    )
+}
+
+export default TextComponent;
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import * as React from "react"
+import Test from '../components/test';
+
+const IndexPage = () => {
+  return (
+    <Test />
+  )
+}
+
+export default IndexPage
